441de2077a0db80b648796392735d41487bb28ea
swapnil-bagul <bagulswapnilp@gmail.com>
2021-09-06 22:54:39 +0530

    store the day & daily and total wage

--- EmpWageProblem.sh
+++ EmpWageProblem.sh
@@ -6,6 +6,7 @@ WagePerHr=20
 TotalWorkingday=20
 TotalWage=0
 Day=i
+declare -a Day
 declare -a DailyWage
 declare -a TotalWage
 for((i=1; i<=20; i++))
@@ -22,15 +23,12 @@ $isFullTime)    WorkingHr=8
 *)              WorkingHr=0
 ;;
 esac
+                Day[i]="$i"
                 Wage=$(($WorkingHr*$WagePerHr))
                 DailyWage[i]="$Wage"
                 TotalWage=$(($TotalWage+$Wage))
                 TotalWage[i]="$TotalWage"
 done
+                echo "Day ${Day[@]}"
                 echo "Daily Wage is "${DailyWage[@]}" "
                 echo "Total wage is "${TotalWage[@]}" "
-
-
-
-
-
