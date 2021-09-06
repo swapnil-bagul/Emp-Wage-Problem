fbc25be9d746696d609698967145da60be36fac6
swapnil-bagul <bagulswapnilp@gmail.com>
2021-09-06 21:57:02 +0530

    To Check Emp attendance

--- EmpWageProblem.sh
+++ EmpWageProblem.sh
@@ -1,1 +1,13 @@
-#Welcome to Employee Wage Computation Program
+#!/bin/bash -x
+#Employee present absent status
+
+        check=$((RANDOM%2))
+
+if [ $check -eq 0 ]
+then
+        status=present
+else
+        status=absent
+fi
+
+echo "Employee is $status"
